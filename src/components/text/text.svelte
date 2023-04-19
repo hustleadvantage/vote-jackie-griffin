753e23c8be5b5cea49b728f71178089372c37f43
Hassan Robinson <24365381+hassanrobinson@users.noreply.github.com>
2023-04-19 19:22:25 -0400

    Removed dark text styles

--- src/components/text/text.svelte
+++ src/components/text/text.svelte
@@ -1,10 +1,10 @@
-<script lang='ts'>
+<script lang="ts">
   export let as: 'p' | 'span' = 'p';
-  export let color: string = 'text-gray-900 dark:text-gray-300';
+  export let color: string = '';
   export let size: string = 'text-lg';
   export { className as class };
 </script>
 
-<svelte:element class={`${size} ${color}`} this={as}>
+<svelte:element this={as} class={`${size} ${color}`}>
   <slot />
 </svelte:element>
