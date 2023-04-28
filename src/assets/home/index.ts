1bebac4ef137b234956344bd08eb6e3888e518a7
Hassan Robinson <24365381+hassanrobinson@users.noreply.github.com>
2023-04-28 16:55:35 -0400

    Updates

--- src/assets/home/index.ts
+++ src/assets/home/index.ts
@@ -1,3 +1,4 @@
+export { default as HomeFlagBackground } from './flag-bg.webp';
 export { default as HomeHeroImage } from './hero.webp';
 export { default as HomeIntroImage } from './jackie.webp';
 export { default as HomeIssuesImage } from './on-the-issues.webp';
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,15 +1,21 @@
+const DONATE_URL = import.meta.env.PUBLIC_DONATE_URL;
+
 export const config = {
+  links: {
+    donate: DONATE_URL,
+    facebook: import.meta.env.PUBLIC_FACEBOOK_URL,
+  },
   navigation: {
     links: [
       { text: 'Home', href: '/' },
       { text: 'Issues', href: '/issues' },
       { text: 'Events', href: '/events' },
-      { text: 'Volunteer', href: '/volunteer' },
+      { text: 'Get Involved', href: '/get-involved' },
       { text: 'About', href: '/about' },
       { text: 'Contact', href: '/contact' },
     ],
     actions: {
-      donate: { url: import.meta.env.PUBLIC_DONATE_URL, text: 'Donate' },
+      donate: { url: DONATE_URL, text: 'Donate' },
     },
   },
 };
